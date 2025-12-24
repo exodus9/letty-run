9394d1a70d2bf9e793a5e7a43ef062c21d8f84a6
daewookoh <daewoo>
2025-12-24 15:17:12 +0900

    maxEidth 430

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,11 +3,13 @@ import LettyRunGame from "@/components/LettyRunGame";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-black flex flex-col">
-      <Navigation />
-      <main className="flex-1">
-        <LettyRunGame />
-      </main>
+    <div className="min-h-screen bg-black flex justify-center">
+      <div className="w-full max-w-[430px] flex flex-col">
+        <Navigation />
+        <main className="flex-1">
+          <LettyRunGame />
+        </main>
+      </div>
     </div>
   );
 };
